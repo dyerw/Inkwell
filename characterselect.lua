b1bfe9f19b51f1414cba17151cfae0f55d367739
wesa <dale.h.wesa@bmwna.com>
2017-04-19 18:04:51 -0500

    oooo rewrote shit

--- characterselect.lua
+++ characterselect.lua
@@ -4,36 +4,71 @@ local scene = composer.newScene()
 local widget = require "widget"
 
 local screenW, screenH, halfW, halfH = display.actualContentWidth, display.actualContentHeight, display.contentCenterX, display.contentCenterY
-local lovecraftButton = nil
+
 local sceneGroup = nil
 local background = nil
 
+local authors = {'will', 'lovecraft'}
+local images = {
+    will = {
+        selected = 'willSelected.png',
+        notSelected = 'willNotSelected.png'
+    },
+    lovecraft = {
+        selected = 'lovecraftSelected.png',
+        notSelected = 'lovecraftNotSelected.png'
+    }
+}
 
-local onLovecraftBtnRelease = nil
+local heights = {
+    will = 150,
+    lovecraft = 350
+}
 
-local function createLovecraftButton(image)
-    if lovecraftButton then
-        lovecraftButton:removeSelf()
+local ogButtons = {}
+local selectedGuy = nil
+
+
+function select(event)
+    if selectedGuy then
+        selectedGuy:removeSelf()
+        selectedGuy = nil
     end
-
-    lovecraftButton = widget.newButton{
-		defaultFile = image,
-		width=150, height=150,
-		onRelease = onLovecraftBtnRelease
-	}
-	lovecraftButton.x = halfW
-	lovecraftButton.y = 150
-	sceneGroup:insert( lovecraftButton )
+    local pressed = event.target
+    local author = pressed.author
+    createButton(author, "selected", heights[author])
 end
 
-onLovecraftBtnRelease = function()
-    print('selected lovecraft')
-    createLovecraftButton('lovecraftSelected.png')
-	return true	-- indicates successful touch
+function createButton(author, buttonState, y)
+
+    if willButton then
+        willButton:removeSelf()
+    end
+
+    print(author)
+    local imageCandidates = images[author]
+    local image = imageCandidates[buttonState]
+    local button = widget.newButton{
+		defaultFile = image,
+		width=150, height=150,
+		onRelease = select
+	}
+	button.x = halfW
+	button.y = y
+    button.author = author
+	sceneGroup:insert( button )
+    if buttonState ~= "selected" then
+        ogButtons[#ogButtons + 1] = button
+    else
+        selectedGuy = button
+    end
 end
 
 function deselect()
-    createLovecraftButton("lovecraftNotSelected.png")
+    if selectedGuy then
+        selectedGuy:removeSelf()
+        selectedGuy = nil
+    end
 end
 
 function createBackground()
@@ -50,7 +85,9 @@ end
 function scene:create( event )
     sceneGroup = self.view
     createBackground()
-    createLovecraftButton('lovecraftNotSelected.png')
+    for index, author in pairs( authors ) do
+        createButton(author, "notSelected", heights[author])
+    end
 end
 
 function scene:show( event )
